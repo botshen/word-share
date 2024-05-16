915168574e6f4959d08f4da19f4680914e9c19cf
Xin shen <363088847@qq.com>
2024-05-16 11:04:13 +0800

    react-quill

--- src/tabs/word-editor.tsx
+++ src/tabs/word-editor.tsx
@@ -1,11 +1,12 @@
-import '@wangeditor/editor/dist/css/style.css' // 引入 css
+import '@wangeditor/editor/dist/css/style.css'; // 引入 css
 
-import React, { useState, useRef, useEffect } from 'react'
-import * as htmlToImage from 'html-to-image';
-import { Editor, Toolbar } from '@wangeditor/editor-for-react'
-import type { IDomEditor, IEditorConfig, IToolbarConfig } from '@wangeditor/editor'
 import { useStorage } from "@plasmohq/storage/hook";
+import * as htmlToImage from 'html-to-image';
+import { useEffect, useRef, useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import { storageConfig } from "~store";
+import { Resizable } from 're-resizable';
 
 function MyEditor() {
   const [word] = useStorage<string>(storageConfig)
@@ -13,44 +14,78 @@ function MyEditor() {
   const imgRef = useRef(null);
 
   // 编辑器内容
-  const [html, setHtml] = useState(word)
+  const [code, setCode] = useState("");
+  const handleProcedureContentChange = (content, delta, source, editor) => {
+    setCode(content);
+    //let has_attribues = delta.ops[1].attributes || "";
+    //console.log(has_attribues);
+    //const cursorPosition = e.quill.getSelection().index;
+    // this.quill.insertText(cursorPosition, "★");
+    //this.quill.setSelection(cursorPosition + 1);
+  };
 
-  const [editor, setEditor] = useState<IDomEditor | null>(null)
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ size: [] }],
+      [{ font: [] }],
+      [{ align: ["right", "center", "justify"] }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image"],
+      [{ color: ["red", "#785412"] }],
+      [{ background: ["red", "#785412"] }]
+    ]
+  };
 
-  // 工具栏配置
-  const toolbarConfig: Partial<IToolbarConfig> = {}
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "link",
+    "color",
+    "image",
+    "background",
+    "align",
+    "size",
+    "font"
+  ];
 
-  // 编辑器配置
-  const editorConfig: Partial<IEditorConfig> = {
-    placeholder: '请输入内容...',
-  }
+
+
+
+
   // 当 word 变更时，更新编辑器内容
   useEffect(() => {
     if (word) {
-      setHtml(`<div>${word}</div>`);
+      setCode(`${word}`);
+      console.log('==============')
     }
   }, [word]);
 
-  // 及时销毁 editor
-  useEffect(() => {
-    return () => {
-      if (editor == null) return
-      editor.destroy()
-      setEditor(null)
-    }
-  }, [editor])
+
 
   // Function to generate image from HTML
   const generateImage = () => {
     const node = nodeRef.current;
+    // const node = document.createElement('div');
+    // node.innerHTML = code + '<p><br></p>'; // add the content to the new node
+
+
     if (node) {
-      const alteredHtml = node.innerHTML + '<p><br></p>'; // 添加额外的底部边距
-      node.innerHTML = alteredHtml;
-      htmlToImage.toPng(node,{
-        
+      // const alteredHtml = node.innerHTML + '<p><br></p>'; // 添加额外的底部边距
+      // node.innerHTML = alteredHtml;
+      console.log('node', node)
+      htmlToImage.toPng(node, {
+
       })
         .then((dataUrl) => {
-          node.innerHTML = html; // 在截图后，重置为原来的HTML 
+          node.innerHTML = code; // 在截图后，重置为原来的HTML 
           const img = new Image();
           img.src = dataUrl;
           if (imgRef.current) {
@@ -60,44 +95,54 @@ function MyEditor() {
         })
         .catch((error) => {
           console.error('故障发生', error);
-          node.innerHTML = html; // 如果发生错误，同样重置HTML
+          node.innerHTML = code; // 如果发生错误，同样重置HTML
 
         });
     }
   }
+  const getHtml = () => {
+    const quill = reactQuillRef.current;
+    console.log('quill', quill)
+
+  }
+  const reactQuillRef = useRef(null); // 创建对ReactQuill组件的引用
+  const [quill, setQuill] = useState(null); // 使用state来持有Quill实例
+
+  useEffect(() => {
+    if (reactQuillRef.current) {
+      setQuill(reactQuillRef.current.getEditor());
+    }
+  }, []);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <div style={{ width: '600px', flexShrink: 0 }}>
+      <Resizable
+        defaultSize={{
+          width: 320,
+          height: 200,
+        }}
+      >
         <div style={{ border: '1px solid #ccc', zIndex: 100 }}>
-          <Toolbar
-            editor={editor}
-            defaultConfig={toolbarConfig}
-            mode="default"
-            style={{ borderBottom: '1px solid #ccc' }}
-          />
-          <Editor
-            defaultConfig={editorConfig}
-            value={html}
-            onCreated={setEditor}
-            onChange={editor => setHtml(editor.getHtml())}
-            mode="default"
-            style={{ height: '500px', overflowY: 'hidden' }}
+          <ReactQuill
+            ref={reactQuillRef}
+
+            theme="snow"
+            modules={modules}
+            formats={formats}
+            value={code}
+            onChange={handleProcedureContentChange}
           />
         </div>
-
-        <div
-          ref={nodeRef}
-          dangerouslySetInnerHTML={{ __html: html }}
-
-        ></div>
-        {html}
+        <div style={{ border: '1px solid #ccc', }}>
+          <div
+            ref={nodeRef}
+            dangerouslySetInnerHTML={{ __html: code }}
+            style={{ padding: '14px', border: '1px solid red' }} // 添加内联样式 
+          />
+        </div>
         <button onClick={generateImage}>Generate Image</button>
-      </div>
-
-      <div style={{ padding: '0 20px' }}>
-        <div ref={imgRef}></div>
-      </div>
+        <button onClick={getHtml}> get html</button>
+      </Resizable>
     </div>
   )
 }
